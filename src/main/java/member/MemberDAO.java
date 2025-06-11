a87c5863cf7b6b85ff7c4f4f840aea52e14f90b3
user <1chlgusqls@g.shingu.ac.kr>
2025-06-11 15:21:02 +0900

    2025.6.11

--- src/main/java/member/MemberDAO.java
+++ src/main/java/member/MemberDAO.java
@@ -22,6 +22,7 @@ public class MemberDAO {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public void close() {
@@ -37,21 +38,22 @@ public class MemberDAO {
 		List<Member> memberlist = new ArrayList<>();
 		// 모든 회원들의 정보를 리턴하는 부분
 		open();
+		
 		try {
-			pstmt = conn.prepareStatement("SELECT * FROM members");
+			pstmt = conn.prepareStatement("SELECT * FROM MEMBERS");
 			ResultSet rs = pstmt.executeQuery();
-			
-		while(rs.next()) {
-			Member m = new Member();
-			m.setId(rs.getInt("id"));
-			m.setUsername(rs.getString("username"));
-			m.setCompany(rs.getString("company"));
-			m.setBirthday(rs.getDate("birthday"));
-			m.setEmail(rs.getString("email"));
-			
-			memberlist.add(m);
-			System.out.println("getall");
+
+			while(rs.next()) {
+				Member m = new Member();
+				m.setId(rs.getInt("id"));
+				m.setUsername(rs.getString("username"));
+				m.setCompany(rs.getString("company"));
+				m.setBirthday(rs.getDate("birthday"));
+				m.setEmail(rs.getString("email"));
+				
+				memberlist.add(m);
 			}
+			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
@@ -63,5 +65,43 @@ public class MemberDAO {
 	
 	public void insert(Member m) {
 		// 새로운 회원을 회원테이블에 집어넣기
+		// 2025.6.11 마지막 수업시간
+		open();
+		String sql = "insert into members(username,company,birthday,email)"
+					+ "values(?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,  m.getUsername());
+			pstmt.setString(2,m.getCompany());
+			pstmt.setDate(3, m.getBirthday());
+			pstmt.setString(4, m.getEmail());
+			
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
